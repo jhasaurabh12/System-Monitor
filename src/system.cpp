7a54b1335ae3cd553bfb840dde0013107b1b1dc3
imsaurabhj <jhasaurabh12@gmail.com>
2020-07-03 16:25:59 +0530

    completed osname,kernel, uptime, total_proc

--- src/system.cpp
+++ src/system.cpp
@@ -3,15 +3,25 @@
 #include <set>
 #include <string>
 #include <vector>
+#include<fstream>
+#include<sstream>
+#include<algorithm>
 
-#include "process.h"
-#include "processor.h"
-#include "system.h"
+#include "../include/process.h"
+#include "../include/processor.h"
+#include "../include/system.h"
 
 using std::set;
 using std::size_t;
 using std::string;
 using std::vector;
+using std::ifstream;
+using std::istringstream;
+
+const string KernelNameFilepath ="/proc/version"; 
+const string OSnameFilePath="/etc/os-release";
+const string StatFilepath="/proc/stat";
+const string UptimeFilepath="/proc/uptime";
 
 // TODO: Return the system's CPU
 Processor& System::Cpu() { return cpu_; }
@@ -19,20 +29,98 @@ Processor& System::Cpu() { return cpu_; }
 // TODO: Return a container composed of the system's processes
 vector<Process>& System::Processes() { return processes_; }
 
-// TODO: Return the system's kernel identifier (string)
-std::string System::Kernel() { return string(); }
-
+// DONE: Return the system's kernel identifier (string)
+std::string System::Kernel() { 
+    
+    string temp1,temp2;
+    string version;
+    ifstream fin(KernelNameFilepath);
+    if(fin)
+    {
+        fin>>temp1>>temp2>>version;
+    }
+    return version;  
+}
 // TODO: Return the system's memory utilization
 float System::MemoryUtilization() { return 0.0; }
 
-// TODO: Return the operating system name
-std::string System::OperatingSystem() { return string(); }
+// DONE: Return the operating system name
+std::string System::OperatingSystem()
+{ 
+    string OSname,Osversion;
+    string temp,temp2,key;
+    ifstream fin(OSnameFilePath);
+    if(fin)
+    {   string line;
+        while(getline(fin,line))
+        {
+            std::replace(line.begin(),line.end(),' ','_');
+            std::replace(line.begin(),line.end(),'=',' ');
+            std::replace(line.begin(),line.end(),'"',' ');
+            istringstream stream(line);
+            if(stream>>key>>OSname)
+            {
+              if(key=="PRETTY_NAME")
+                break;
+            }
+            
+        }
+        std::replace(OSname.begin(),OSname.end(),'_',' ');
+    }
+    return OSname; 
+}
 
-// TODO: Return the number of processes actively running on the system
-int System::RunningProcesses() { return 0; }
+// DONE: Return the number of processes actively running on the system
+int System::RunningProcesses() { 
+    string key,line,running;
+    ifstream fin(StatFilepath);
+    while(fin)
+    {
+        getline(fin,line);
+        istringstream stream(line);
+        if(stream>>key)
+        {
+            if(key=="procs_running")
+            {
+                stream>>running;
+                break;
+            }
+        }
+    }  
+    return stoi(running); 
+}
 
-// TODO: Return the total number of processes on the system
-int System::TotalProcesses() { return 0; }
+// DONE: Return the total number of processes on the system
+int System::TotalProcesses() { 
+   string key,line,totalproc;
+    ifstream fin(StatFilepath);
+    while(fin)
+    {
+        getline(fin,line);
+        istringstream stream(line);
+        if(stream>>key)
+        {
+            if(key=="processes")
+            {
+                stream>>totalproc;
+                break;
+            }
+        }
+    }  
+    return stoi(totalproc); 
+}
 
-// TODO: Return the number of seconds since the system started running
-long int System::UpTime() { return 0; }
+// DONE: Return the number of seconds since the system started running
+long int System::UpTime() { 
+    string idle,uptime="0";
+    ifstream fin(UptimeFilepath);
+    if(fin)
+    {
+        string line;
+        getline(fin,line);
+        istringstream stream(line);
+        stream>>uptime>>idle;
+        
+    }
+    return stol(uptime);
+}
